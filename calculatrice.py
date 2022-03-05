790f67e0301972d7cc28469f26d60caaf1b3ae73
alichowdhury01 <alichowdhury01@gmail.com>
2022-03-05 14:15:23 -0500

    Update calculatrice.py

--- calculatrice.py
+++ calculatrice.py
@@ -1,14 +1,16 @@
-#Fonction calculatrice
+#Exercice 2, Partie 1:
+
+#Fonction calculatrice:
 def calculatrice(a, b, c, d):
-    #Partie addition
+    #Partie addition:
     adition = a/b + d/c
-    #Partie soustration
+    #Partie soustration:
     soustraction = a/b - d/c
-    #Partie multiplication 
+    #Partie multiplication:
     multiplication = a/b * d/c
-    #Partie division 
+    #Partie division:
     divisions = a/b / d/c
-    #Retoune chaque valeur des operations
+    #Retoune chaque valeur des operations:
     return adition, soustraction, multiplication, divisions
-#Affriche la function calculatrice en attribuent le int disiré dans le print aux paramètre de la function 
+#Affriche le résultat de la function calculatrice en attribuent les int disiré dans les paramètres de la function:
 print(calculatrice(2, 1, 8, 4))
