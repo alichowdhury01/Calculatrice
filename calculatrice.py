29d88f09c52ca85b1912a87636fb75acefaaeea1
alichowdhury01 <alichowdhury01@gmail.com>
2022-03-05 14:35:14 -0500

    Update calculatrice.py

--- calculatrice.py
+++ calculatrice.py
@@ -1,6 +1,9 @@
 #Exercice 2, Partie 1:
 
 #Fonction calculatrice:
+from calendar import c
+
+
 def calculatrice(a, b, c, d):
     #Partie addition:
     adition = a/b + d/c
@@ -14,3 +17,19 @@ def calculatrice(a, b, c, d):
     return adition, soustraction, multiplication, divisions
 #Affriche le résultat de la function calculatrice en attribuent les int disiré dans les paramètres de la function:
 print(calculatrice(2, 1, 8, 4))
+
+#Exercice 2, Partie 2:
+
+def calculatrice_2(fraction1, fraction2):
+    #Partie addition:
+    adition = fraction1 + fraction2
+    #Partie soustration:
+    soustraction = fraction1 - fraction2
+    #Partie multiplication:
+    multiplication = fraction1 * fraction2
+    #Partie division:
+    divisions = fraction1 / fraction2
+    #Retoune chaque valeur des operations:
+    return adition, soustraction, multiplication, divisions
+#Affriche le résultat de la function calculatrice2 en attribuent les int disiré dans les paramètres de la function:
+print(calculatrice_2(2/5, 7/9))
